d86948b7209449c6677a4b2f2b05e8f0d3669883
Foge65 <mr.texnik666@gmail.com>
2024-04-16 21:20:42 +0300

    Refactoring input fields

---
+++ src/main/resources/static/InputField.js
@@ -0,0 +1,17 @@
+const setFields = [
+    "dollarsPerHour1",
+    "hoursPerDay1",
+    "daysPerMonth1",
+
+    "chipsEV2",
+    "rakeBackTotal2",
+    "tourneyPerPeriod2",
+
+    "chipsEV3",
+    "tourneyPerDay3",
+    "rakeBackDollarsPerDay3",
+    "daysPerWeek3",
+    "rakeBackDollarsPerWeek3",
+    "weeksPerPeriod3",
+    "rakeBackDollarsPerPeriod3"
+];
