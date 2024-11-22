9c8bfb51e14945e69730f1e85ef12ed7cbababb6
Foge65 <mr.texnik666@gmail.com>
2024-11-22 17:01:08 +0300

    update buyIns after select room

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -23,63 +23,96 @@ document.addEventListener("DOMContentLoaded", () => {
     const rakebackPct2 = document.getElementById("rakebackPct2");
     const dollarsPerHour2 = document.getElementById("dollarsPerHour2");
 
-    function updateValue(id, url) {
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(`Error response: ${response.status}`);
-                }
-                return response.text();
-            })
-            .then(data => {
-                const value = parseFloat(data);
-                id.value = value.toFixed(2);
-            })
-            .catch(error => {
-                console.error(`Error updating:, ${error.status}`);
-                id.value = "Error";
-            });
-    }
+    fetch('/api/desiredProfit/getData')
+        .then(response => response.json())
+        .then(data => {
+            const roomOption = Array.from(room1.options).find(option => option.value === data.room.name);
+            if (roomOption) {
+                room1.value = roomOption.value;
+            }
 
-    room1.addEventListener("change", () => updateValue(expDollarEVT1, '/api/desiredProfit/getExpEVT'));
-    buyIn1.addEventListener("change", () => updateValue(expDollarEVT1, '/api/desiredProfit/getExpEVT'));
+            const buyInOption = Array.from(buyIn1.options).find(option => option.value === data.buyIn);
+            if (buyInOption) {
+                buyIn1.value = buyInOption.value;
+            }
+        })
+        .catch(error => console.error('Error:', error));
+
+    fetch('/api/haveHoursPerMonth/getData')
+        .then(response => response.json())
+        .then(data => {
+            const roomOption = Array.from(room2.options).find(option => option.value === data.room.name);
+            if (roomOption) {
+                room2.value = roomOption.value;
+            }
+
+            const buyInOption = Array.from(buyIn2.options).find(option => option.value === data.buyIn);
+            if (buyInOption) {
+                buyIn2.value = buyInOption.value;
+            }
+        })
+        .catch(error => console.error('Error:', error));
+
+    room1.addEventListener("change", () => {
+        updateList(buyIn1, '/api/desiredProfit/getBuyIns');
+        updateValue(expDollarEVT1, '/api/desiredProfit/getExpEVT')
+    });
+
+    buyIn1.addEventListener("change", () => {
+        updateValue(rollbackPct1, '/api/desiredProfit/getRollback');
+        updateValue(requiredTourneys1, '/api/desiredProfit/getRequiredTourneys');
+    });
+
     expChipsT1.addEventListener("input", () => updateValue(expDollarEVT1, '/api/desiredProfit/getExpEVT'));
 
-    buyIn1.addEventListener("change", () => updateValue(rollbackPct1, '/api/desiredProfit/getRollback'));
-    desiredProfit.addEventListener("input", () => updateValue(rollbackPct1, '/api/desiredProfit/getRollback'));
+    desiredProfit.addEventListener("input", () => {
+        updateValue(rollbackPct1, '/api/desiredProfit/getRollback');
+        updateValue(requiredTourneys1, '/api/desiredProfit/getRequiredTourneys');
+        updateValue(dollarsPerHour1, '/api/desiredProfit/getDollarPerHour');
+    });
 
-
-    desiredProfit.addEventListener("input", () => updateValue(requiredTourneys1, '/api/desiredProfit/getRequiredTourneys'));
     rollbackPct1.addEventListener("input", () => updateValue(requiredTourneys1, '/api/desiredProfit/getRequiredTourneys'));
-    buyIn1.addEventListener("change", () => updateValue(requiredTourneys1, '/api/desiredProfit/getRequiredTourneys'));
+
     rakebackPct1.addEventListener("input", () => updateValue(requiredTourneys1, '/api/desiredProfit/getRequiredTourneys'));
     expDollarEVT1.addEventListener("input", () => updateValue(requiredTourneys1, '/api/desiredProfit/getRequiredTourneys'));
 
     requiredTourneys1.addEventListener("input", () => updateValue(requiredHours1, '/api/desiredProfit/getRequiredHours'));
     tables1.addEventListener("input", () => updateValue(requiredHours1, '/api/desiredProfit/getRequiredHours'));
 
-    desiredProfit.addEventListener("input", () => updateValue(dollarsPerHour1, '/api/desiredProfit/getDollarPerHour'));
     requiredHours1.addEventListener("input", () => updateValue(dollarsPerHour1, '/api/desiredProfit/getDollarPerHour'));
 
 
-    room2.addEventListener("change", () => updateValue(expDollarEVT2, '/api/haveHoursPerMonth/getExpEVT'));
-    buyIn2.addEventListener("change", () => updateValue(expDollarEVT2, '/api/haveHoursPerMonth/getExpEVT'));
+    room2.addEventListener("change", () => {
+        updateList(buyIn2, '/api/haveHoursPerMonth/getBuyIns');
+        updateValue(expDollarEVT2, '/api/haveHoursPerMonth/getExpEVT');
+    });
+
+    buyIn2.addEventListener("change", () => {
+        updateValue(expDollarEVT2, '/api/haveHoursPerMonth/getExpEVT');
+        updateValue(rollbackPct2, '/api/haveHoursPerMonth/getRollback');
+        updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation');
+    });
+
     expChipsT2.addEventListener("input", () => updateValue(expDollarEVT2, '/api/haveHoursPerMonth/getExpEVT'));
 
-    buyIn2.addEventListener("change", () => updateValue(rollbackPct2, '/api/haveHoursPerMonth/getRollback'));
-    estimatedExpectation.addEventListener("input", () => updateValue(rollbackPct2, '/api/haveHoursPerMonth/getRollback'));
+    estimatedExpectation.addEventListener("input", () => {
+        updateValue(rollbackPct2, '/api/haveHoursPerMonth/getRollback');
+        updateValue(dollarsPerHour2, '/api/haveHoursPerMonth/getDollarPerHour');
+    });
 
-    haveHours.addEventListener("input", () => updateValue(requiredTourneys2, '/api/haveHoursPerMonth/getRequiredTourneys'));
-    tables2.addEventListener("input", () => updateValue(requiredTourneys2, '/api/haveHoursPerMonth/getRequiredTourneys'));
+    haveHours.addEventListener("input", () => {
+        updateValue(requiredTourneys2, '/api/haveHoursPerMonth/getRequiredTourneys');
+        updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation');
+        updateValue(dollarsPerHour2, '/api/haveHoursPerMonth/getDollarPerHour');
+    });
+
+    tables2.addEventListener("input", () => {
+        updateValue(requiredTourneys2, '/api/haveHoursPerMonth/getRequiredTourneys');
+        updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation');
+    });
 
     expDollarEVT2.addEventListener("input", () => updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation'));
-    buyIn2.addEventListener("change", () => updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation'));
     rakebackPct2.addEventListener("input", () => updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation'));
-    tables2.addEventListener("input", () => updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation'));
-    haveHours.addEventListener("input", () => updateValue(estimatedExpectation, '/api/haveHoursPerMonth/getEstimatedExpectation'));
-
-    estimatedExpectation.addEventListener("input", () => updateValue(dollarsPerHour2, '/api/haveHoursPerMonth/getDollarPerHour'));
-    haveHours.addEventListener("input", () => updateValue(dollarsPerHour2, '/api/haveHoursPerMonth/getDollarPerHour'));
 });
 
 function setObjectValue(url, key, value) {
@@ -97,5 +130,48 @@ function setObjectValue(url, key, value) {
         })
         .catch(error => {
             console.error('Error setting value to object:', error);
+        });
+}
+
+function updateValue(id, url) {
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error response: ${response.status}`);
+            }
+            return response.text();
+        })
+        .then(data => {
+            const value = parseFloat(data);
+            id.value = value.toFixed(2);
+        })
+        .catch(error => {
+            console.error(`Error updating:, ${error.status}`);
+            id.value = "Error";
+        });
+}
+
+function updateList(id, url) {
+    id.innerHTML = '';
+
+    fetch(url, {
+        method: 'GET'
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`Error response: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => {
+            data.forEach(element => {
+                const option = document.createElement("option");
+                option.value = element;
+                option.textContent = element;
+                id.appendChild(option);
+            });
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
         });
 }
