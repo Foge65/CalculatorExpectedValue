4bfa0d024d5eaa70e4b213456c1671e7eb63aba3
Foge65 <mr.texnik666@gmail.com>
2024-11-23 17:32:45 +0300

    Refactor DesiredProfitController

--- spring/src/main/java/team/firestorm/controller/DesiredProfitController.java
+++ spring/src/main/java/team/firestorm/controller/DesiredProfitController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import team.firestorm.dto.DesiredProfitRequestDTO;
 import team.firestorm.model.DesiredProfitModel;
+import team.firestorm.service.DesiredProfitService;
 import team.firestorm.service.mesh.*;
 import team.firestorm.service.room.*;
 
@@ -15,6 +16,7 @@ import java.util.stream.IntStream;
 @RequestMapping("/api/desiredProfit")
 @RequiredArgsConstructor
 public class DesiredProfitController {
+    private final DesiredProfitService service;
     private final DesiredProfitModel model;
     private Room room;
     private Mesh mesh;
@@ -22,21 +24,31 @@ public class DesiredProfitController {
     @PostConstruct
     public void init() {
         model.setRooms(Rooms.values());
+        initializeRoom();
+        initializeMesh();
+    }
 
+    private void initializeRoom() {
         if (room == null) {
             room = new PokerStars();
-            model.setRoom(room);
-            model.setBuyIns(room.buyIns());
-            model.setRakes(room.rakes());
-            model.setWinCoefficients(room.winCoefficient());
-            model.setLoseCoefficients(room.loseCoefficient());
-            model.setTourneysPerTable(room.tourneysPerTable());
+            propertiesRoom();
 
             DesiredProfitRequestDTO requestDTO = new DesiredProfitRequestDTO();
             requestDTO.setBuyIn(0.25);
             setBuyIn(requestDTO);
         }
+    }
 
+    private void propertiesRoom() {
+        model.setRoom(room);
+        model.setBuyIns(room.buyIns());
+        model.setRakes(room.rakes());
+        model.setWinCoefficients(room.winCoefficient());
+        model.setLoseCoefficients(room.loseCoefficient());
+        model.setTourneysPerTable(room.tourneysPerTable());
+    }
+
+    private void initializeMesh() {
         if (mesh == null) {
             model.setMeshes(Meshes.values());
             mesh = new ClearProfit();
@@ -57,25 +69,17 @@ public class DesiredProfitController {
     @PostMapping("/setRoom")
     public void setRoom(@RequestBody Map<String, String> request) {
         Rooms rooms = Rooms.valueOf(request.get("room"));
+        createRoom(rooms);
 
+        propertiesRoom();
+    }
+
+    private void createRoom(Rooms rooms) {
         switch (rooms) {
-            case PokerStars:
-                room = new PokerStars();
-                break;
-            case Winamax:
-                room = new Winamax();
-                break;
-            case iPoker:
-                room = new IPoker();
-                break;
+            case PokerStars -> new PokerStars();
+            case Winamax -> new Winamax();
+            case iPoker -> new IPoker();
         }
-
-        model.setRoom(room);
-        model.setBuyIns(room.buyIns());
-        model.setRakes(room.rakes());
-        model.setWinCoefficients(room.winCoefficient());
-        model.setLoseCoefficients(room.loseCoefficient());
-        model.setTourneysPerTable(room.tourneysPerTable());
     }
 
     @PostMapping("/setBuyIn")
@@ -87,6 +91,10 @@ public class DesiredProfitController {
         model.setRake(room.rakes()[index]);
         model.setWinCoefficient(room.winCoefficient()[index]);
         model.setLoseCoefficient(room.loseCoefficient()[index]);
+    }
+
+    private int findIndexBySelectedCoefficient(double[] buyIns, double buyIn) {
+        return IntStream.range(0, buyIns.length).filter(i -> buyIns[i] == buyIn).findFirst().orElse(0);
     }
 
     @GetMapping("/getBuyIns")
@@ -101,17 +109,7 @@ public class DesiredProfitController {
 
     @GetMapping("/getExpEVT")
     public double getExpEVT() {
-        double buyIn = model.getBuyIn();
-        double chipsEV = model.getExpChipsT();
-        double winCoefficient = model.getWinCoefficient();
-        double loseCoefficient = model.getLoseCoefficient();
-
-        double expEVT = buyIn * 1 * (((500 + chipsEV) / 1500) * winCoefficient
-                                     + (1 - ((500 + chipsEV) / 1500)) * loseCoefficient);
-
-        model.setExpDollarEVT(expEVT);
-
-        return expEVT;
+        return service.expDollarEVT();
     }
 
     @PostMapping("/setTables")
@@ -128,212 +126,36 @@ public class DesiredProfitController {
     public void setMesh(@RequestBody Map<String, String> request) {
         Meshes meshes = Meshes.valueOf(request.get("mesh"));
 
-        switch (meshes) {
-            case BackingWithStudy:
-                mesh = new BackingWithStudy();
-                break;
-            case BackingWithoutStudy:
-                mesh = new BackingWithoutStudy();
-                break;
-            case StudyWithoutBacking:
-                mesh = new StudyWithoutBacking();
-                break;
-        }
+        initializeMesh(meshes);
         model.setMesh(mesh);
+    }
+
+    private void initializeMesh(Meshes meshes) {
+        switch (meshes) {
+            case ClearProfit -> new ClearProfit();
+            case BackingWithStudy -> new BackingWithStudy();
+            case BackingWithoutStudy -> new BackingWithoutStudy();
+        }
     }
 
     @GetMapping("/getRollback")
     public double getRollback() {
-        double buyIn = model.getBuyIn();
-        double evBI = model.getDesiredProfit() / buyIn;
-
-        int rollback = 0;
-
-        if (mesh instanceof BackingWithStudy) {
-            if (buyIn < 5) {
-                if (evBI >= 225) rollback = 60;
-                if (evBI >= 199 && evBI <= 224) rollback = 59;
-                if (evBI >= 176 && evBI <= 189) rollback = 57;
-                if (evBI >= 150 && evBI <= 175) rollback = 56;
-                if (evBI >= 120 && evBI <= 149) rollback = 50;
-                if (evBI >= 90 && evBI <= 119) rollback = 48;
-                if (evBI >= 66 && evBI <= 89) rollback = 41;
-                if (evBI >= 40 && evBI <= 65) rollback = 40;
-                if (evBI < 40) rollback = 40;
-            }
-            if (buyIn >= 5 && buyIn < 10) {
-                if (evBI >= 225) rollback = 62;
-                if (evBI >= 199 && evBI <= 224) rollback = 61;
-                if (evBI >= 176 && evBI <= 189) rollback = 59;
-                if (evBI >= 150 && evBI <= 175) rollback = 56;
-                if (evBI >= 120 && evBI <= 149) rollback = 55;
-                if (evBI >= 90 && evBI <= 119) rollback = 51;
-                if (evBI >= 66 && evBI <= 89) rollback = 46;
-                if (evBI >= 40 && evBI <= 65) rollback = 42;
-                if (evBI < 40) rollback = 40;
-            }
-            if (buyIn >= 10 && buyIn < 25) {
-                if (evBI >= 225) rollback = 65;
-                if (evBI >= 199 && evBI <= 224) rollback = 64;
-                if (evBI >= 176 && evBI <= 189) rollback = 62;
-                if (evBI >= 150 && evBI <= 175) rollback = 61;
-                if (evBI >= 120 && evBI <= 149) rollback = 55;
-                if (evBI >= 90 && evBI <= 119) rollback = 53;
-                if (evBI >= 66 && evBI <= 89) rollback = 46;
-                if (evBI >= 40 && evBI <= 65) rollback = 42;
-                if (evBI < 40) rollback = 40;
-            }
-            if (buyIn >= 25 && buyIn < 50) {
-                if (evBI >= 225) rollback = 70;
-                if (evBI >= 199 && evBI <= 224) rollback = 69;
-                if (evBI >= 176 && evBI <= 189) rollback = 67;
-                if (evBI >= 150 && evBI <= 175) rollback = 64;
-                if (evBI >= 120 && evBI <= 149) rollback = 60;
-                if (evBI >= 90 && evBI <= 119) rollback = 58;
-                if (evBI >= 66 && evBI <= 89) rollback = 51;
-                if (evBI >= 40 && evBI <= 65) rollback = 48;
-                if (evBI < 40) rollback = 45;
-            }
-            if (buyIn >= 50 && buyIn < 100) {
-                if (evBI >= 225) rollback = 80;
-                if (evBI >= 199 && evBI <= 224) rollback = 79;
-                if (evBI >= 176 && evBI <= 189) rollback = 77;
-                if (evBI >= 150 && evBI <= 175) rollback = 74;
-                if (evBI >= 120 && evBI <= 149) rollback = 70;
-                if (evBI >= 90 && evBI <= 119) rollback = 68;
-                if (evBI >= 66 && evBI <= 89) rollback = 61;
-                if (evBI >= 40 && evBI <= 65) rollback = 55;
-                if (evBI < 40) rollback = 50;
-            }
-            if (buyIn >= 100) {
-                if (evBI >= 225) rollback = 83;
-                if (evBI >= 199 && evBI <= 224) rollback = 82;
-                if (evBI >= 176 && evBI <= 189) rollback = 80;
-                if (evBI >= 150 && evBI <= 175) rollback = 76;
-                if (evBI >= 120 && evBI <= 149) rollback = 75;
-                if (evBI >= 90 && evBI <= 119) rollback = 73;
-                if (evBI >= 66 && evBI <= 89) rollback = 66;
-                if (evBI >= 40 && evBI <= 65) rollback = 60;
-                if (evBI < 40) rollback = 60;
-            }
-        }
-
-        if (mesh instanceof BackingWithoutStudy) {
-            if (buyIn >= 5 && buyIn < 10) {
-                if (evBI >= 280) rollback = 90;
-                if (evBI >= 210 && evBI <= 279) rollback = 87;
-                if (evBI >= 180 && evBI <= 209) rollback = 86;
-                if (evBI >= 155 && evBI <= 179) rollback = 85;
-                if (evBI >= 125 && evBI <= 154) rollback = 84;
-                if (evBI >= 95 && evBI <= 124) rollback = 82;
-                if (evBI >= 70 && evBI <= 89) rollback = 80;
-                if (evBI >= 55 && evBI <= 69) rollback = 78;
-                if (evBI < 55) rollback = 12;
-            }
-            if (buyIn >= 10 && buyIn < 25) {
-                if (evBI >= 280) rollback = 95;
-                if (evBI >= 195 && evBI <= 279) rollback = 93;
-                if (evBI >= 145 && evBI <= 194) rollback = 91;
-                if (evBI >= 105 && evBI <= 144) rollback = 88;
-                if (evBI >= 80 && evBI <= 104) rollback = 85;
-                if (evBI >= 65 && evBI <= 79) rollback = 82;
-                if (evBI >= 50 && evBI <= 64) rollback = 79;
-                if (evBI >= 40 && evBI <= 49) rollback = 76;
-                if (evBI < 40) rollback = 8;
-            }
-            if (buyIn >= 25) {
-                if (evBI >= 240) rollback = 95;
-                if (evBI >= 190 && evBI <= 239) rollback = 94;
-                if (evBI >= 140 && evBI <= 189) rollback = 92;
-                if (evBI >= 120 && evBI <= 139) rollback = 91;
-                if (evBI >= 105 && evBI <= 119) rollback = 90;
-                if (evBI >= 85 && evBI <= 104) rollback = 88;
-                if (evBI >= 70 && evBI <= 84) rollback = 86;
-                if (evBI >= 50 && evBI <= 69) rollback = 85;
-                if (evBI < 50) rollback = 6;
-            }
-        }
-
-        if (mesh instanceof StudyWithoutBacking) {
-            if (buyIn < 5) {
-                if (evBI >= 225) rollback = 80;
-                if (evBI >= 190 && evBI <= 224) rollback = 79;
-                if (evBI >= 176 && evBI <= 189) rollback = 77;
-                if (evBI >= 150 && evBI <= 175) rollback = 76;
-                if (evBI >= 120 && evBI <= 149) rollback = 70;
-                if (evBI >= 90 && evBI <= 119) rollback = 68;
-                if (evBI >= 66 && evBI <= 89) rollback = 61;
-                if (evBI <= 65) rollback = 25;
-            }
-            if (buyIn >= 5 && buyIn < 10) {
-                if (evBI >= 225) rollback = 82;
-                if (evBI >= 190 && evBI <= 224) rollback = 81;
-                if (evBI >= 176 && evBI <= 189) rollback = 79;
-                if (evBI >= 150 && evBI <= 175) rollback = 76;
-                if (evBI >= 120 && evBI <= 149) rollback = 75;
-                if (evBI >= 90 && evBI <= 119) rollback = 71;
-                if (evBI >= 66 && evBI <= 89) rollback = 66;
-                if (evBI >= 56 && evBI <= 65) rollback = 64;
-                if (evBI <= 55) rollback = 20;
-            }
-            if (buyIn >= 10) {
-                if (evBI >= 225) rollback = 85;
-                if (evBI >= 190 && evBI <= 224) rollback = 84;
-                if (evBI >= 176 && evBI <= 189) rollback = 82;
-                if (evBI >= 150 && evBI <= 175) rollback = 81;
-                if (evBI >= 120 && evBI <= 149) rollback = 75;
-                if (evBI >= 90 && evBI <= 119) rollback = 73;
-                if (evBI >= 66 && evBI <= 89) rollback = 66;
-                if (evBI >= 56 && evBI <= 65) rollback = 62;
-                if (evBI <= 55) rollback = 20;
-            }
-        }
-
-        if (mesh instanceof ClearProfit) {
-            rollback = 100;
-        }
-
-        model.setRollback(rollback);
-
-        return rollback;
+        return service.rollback();
     }
 
     @GetMapping("/getRequiredTourneys")
     public double getRequiredTourneys() {
-        double desiredProfit = model.getDesiredProfit();
-        double buyIn = model.getBuyIn();
-        double rollback = model.getRollback();
-        double rake = model.getRake();
-        double rakeback = model.getRakebackPct();
-        double dollarEVPerTourney = model.getExpDollarEVT();
-        double requiredTourneys = desiredProfit / (rollback / 100) / (buyIn * (rake / 100) * (rakeback / 100)
-                                                                      + dollarEVPerTourney) + 1;
-
-        model.setRequiredTourneys(requiredTourneys);
-
-        return requiredTourneys;
+        return service.requiredTourneys();
     }
 
     @GetMapping("/getRequiredHours")
     public double getRequiredHours() {
-        double requiredHours = model.getRequiredTourneys() / (model.getTables() * model.getTourneysPerTable());
-
-        model.setRequiredHours(requiredHours);
-
-        return requiredHours;
+        return service.requiredHours();
     }
 
     @GetMapping("/getDollarPerHour")
     public double getDollarPerHour() {
-        double dollarsPerHour = model.getDesiredProfit() / model.getRequiredHours();
-
-        model.setDollarPerHour(dollarsPerHour);
-
-        return dollarsPerHour;
-    }
-
-    private int findIndexBySelectedCoefficient(double[] buyIns, double buyIn) {
-        return IntStream.range(0, buyIns.length).filter(i -> buyIns[i] == buyIn).findFirst().orElse(0);
+        return service.dollarPerHour();
     }
 
 }
---
+++ spring/src/main/java/team/firestorm/service/DesiredProfitService.java
@@ -0,0 +1,210 @@
+package team.firestorm.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import team.firestorm.model.DesiredProfitModel;
+import team.firestorm.service.mesh.*;
+import team.firestorm.service.room.Room;
+
+@Service
+@Getter
+@RequiredArgsConstructor
+public class DesiredProfitService {
+    private final DesiredProfitModel model;
+    private final Room room;
+    private final Mesh mesh;
+
+    public double expDollarEVT() {
+        double buyIn = model.getBuyIn();
+        double chipsEV = model.getExpChipsT();
+        double winCoefficient = model.getWinCoefficient();
+        double loseCoefficient = model.getLoseCoefficient();
+
+        double expEVT = buyIn * 1 * (((500 + chipsEV) / 1500) * winCoefficient
+                                     + (1 - ((500 + chipsEV) / 1500)) * loseCoefficient);
+
+        model.setExpDollarEVT(expEVT);
+
+        return expEVT;
+    }
+
+    public double rollback() {
+        double buyIn = model.getBuyIn();
+        double evBI = model.getDesiredProfit() / buyIn;
+        int rollback = 0;
+
+        if (mesh instanceof BackingWithStudy) {
+            if (buyIn < 5) {
+                if (evBI >= 225) rollback = 60;
+                if (evBI >= 199 && evBI <= 224) rollback = 59;
+                if (evBI >= 176 && evBI <= 189) rollback = 57;
+                if (evBI >= 150 && evBI <= 175) rollback = 56;
+                if (evBI >= 120 && evBI <= 149) rollback = 50;
+                if (evBI >= 90 && evBI <= 119) rollback = 48;
+                if (evBI >= 66 && evBI <= 89) rollback = 41;
+                if (evBI >= 40 && evBI <= 65) rollback = 40;
+                if (evBI < 40) rollback = 40;
+            }
+            if (buyIn >= 5 && buyIn < 10) {
+                if (evBI >= 225) rollback = 62;
+                if (evBI >= 199 && evBI <= 224) rollback = 61;
+                if (evBI >= 176 && evBI <= 189) rollback = 59;
+                if (evBI >= 150 && evBI <= 175) rollback = 56;
+                if (evBI >= 120 && evBI <= 149) rollback = 55;
+                if (evBI >= 90 && evBI <= 119) rollback = 51;
+                if (evBI >= 66 && evBI <= 89) rollback = 46;
+                if (evBI >= 40 && evBI <= 65) rollback = 42;
+                if (evBI < 40) rollback = 40;
+            }
+            if (buyIn >= 10 && buyIn < 25) {
+                if (evBI >= 225) rollback = 65;
+                if (evBI >= 199 && evBI <= 224) rollback = 64;
+                if (evBI >= 176 && evBI <= 189) rollback = 62;
+                if (evBI >= 150 && evBI <= 175) rollback = 61;
+                if (evBI >= 120 && evBI <= 149) rollback = 55;
+                if (evBI >= 90 && evBI <= 119) rollback = 53;
+                if (evBI >= 66 && evBI <= 89) rollback = 46;
+                if (evBI >= 40 && evBI <= 65) rollback = 42;
+                if (evBI < 40) rollback = 40;
+            }
+            if (buyIn >= 25 && buyIn < 50) {
+                if (evBI >= 225) rollback = 70;
+                if (evBI >= 199 && evBI <= 224) rollback = 69;
+                if (evBI >= 176 && evBI <= 189) rollback = 67;
+                if (evBI >= 150 && evBI <= 175) rollback = 64;
+                if (evBI >= 120 && evBI <= 149) rollback = 60;
+                if (evBI >= 90 && evBI <= 119) rollback = 58;
+                if (evBI >= 66 && evBI <= 89) rollback = 51;
+                if (evBI >= 40 && evBI <= 65) rollback = 48;
+                if (evBI < 40) rollback = 45;
+            }
+            if (buyIn >= 50 && buyIn < 100) {
+                if (evBI >= 225) rollback = 80;
+                if (evBI >= 199 && evBI <= 224) rollback = 79;
+                if (evBI >= 176 && evBI <= 189) rollback = 77;
+                if (evBI >= 150 && evBI <= 175) rollback = 74;
+                if (evBI >= 120 && evBI <= 149) rollback = 70;
+                if (evBI >= 90 && evBI <= 119) rollback = 68;
+                if (evBI >= 66 && evBI <= 89) rollback = 61;
+                if (evBI >= 40 && evBI <= 65) rollback = 55;
+                if (evBI < 40) rollback = 50;
+            }
+            if (buyIn >= 100) {
+                if (evBI >= 225) rollback = 83;
+                if (evBI >= 199 && evBI <= 224) rollback = 82;
+                if (evBI >= 176 && evBI <= 189) rollback = 80;
+                if (evBI >= 150 && evBI <= 175) rollback = 76;
+                if (evBI >= 120 && evBI <= 149) rollback = 75;
+                if (evBI >= 90 && evBI <= 119) rollback = 73;
+                if (evBI >= 66 && evBI <= 89) rollback = 66;
+                if (evBI >= 40 && evBI <= 65) rollback = 60;
+                if (evBI < 40) rollback = 60;
+            }
+        }
+
+        if (mesh instanceof BackingWithoutStudy) {
+            if (buyIn >= 5 && buyIn < 10) {
+                if (evBI >= 280) rollback = 90;
+                if (evBI >= 210 && evBI <= 279) rollback = 87;
+                if (evBI >= 180 && evBI <= 209) rollback = 86;
+                if (evBI >= 155 && evBI <= 179) rollback = 85;
+                if (evBI >= 125 && evBI <= 154) rollback = 84;
+                if (evBI >= 95 && evBI <= 124) rollback = 82;
+                if (evBI >= 70 && evBI <= 89) rollback = 80;
+                if (evBI >= 55 && evBI <= 69) rollback = 78;
+                if (evBI < 55) rollback = 12;
+            }
+            if (buyIn >= 10 && buyIn < 25) {
+                if (evBI >= 280) rollback = 95;
+                if (evBI >= 195 && evBI <= 279) rollback = 93;
+                if (evBI >= 145 && evBI <= 194) rollback = 91;
+                if (evBI >= 105 && evBI <= 144) rollback = 88;
+                if (evBI >= 80 && evBI <= 104) rollback = 85;
+                if (evBI >= 65 && evBI <= 79) rollback = 82;
+                if (evBI >= 50 && evBI <= 64) rollback = 79;
+                if (evBI >= 40 && evBI <= 49) rollback = 76;
+                if (evBI < 40) rollback = 8;
+            }
+            if (buyIn >= 25) {
+                if (evBI >= 240) rollback = 95;
+                if (evBI >= 190 && evBI <= 239) rollback = 94;
+                if (evBI >= 140 && evBI <= 189) rollback = 92;
+                if (evBI >= 120 && evBI <= 139) rollback = 91;
+                if (evBI >= 105 && evBI <= 119) rollback = 90;
+                if (evBI >= 85 && evBI <= 104) rollback = 88;
+                if (evBI >= 70 && evBI <= 84) rollback = 86;
+                if (evBI >= 50 && evBI <= 69) rollback = 85;
+                if (evBI < 50) rollback = 6;
+            }
+        }
+
+        if (mesh instanceof StudyWithoutBacking) {
+            if (buyIn < 5) {
+                if (evBI >= 225) rollback = 80;
+                if (evBI >= 190 && evBI <= 224) rollback = 79;
+                if (evBI >= 176 && evBI <= 189) rollback = 77;
+                if (evBI >= 150 && evBI <= 175) rollback = 76;
+                if (evBI >= 120 && evBI <= 149) rollback = 70;
+                if (evBI >= 90 && evBI <= 119) rollback = 68;
+                if (evBI >= 66 && evBI <= 89) rollback = 61;
+                if (evBI <= 65) rollback = 25;
+            }
+            if (buyIn >= 5 && buyIn < 10) {
+                if (evBI >= 225) rollback = 82;
+                if (evBI >= 190 && evBI <= 224) rollback = 81;
+                if (evBI >= 176 && evBI <= 189) rollback = 79;
+                if (evBI >= 150 && evBI <= 175) rollback = 76;
+                if (evBI >= 120 && evBI <= 149) rollback = 75;
+                if (evBI >= 90 && evBI <= 119) rollback = 71;
+                if (evBI >= 66 && evBI <= 89) rollback = 66;
+                if (evBI >= 56 && evBI <= 65) rollback = 64;
+                if (evBI <= 55) rollback = 20;
+            }
+            if (buyIn >= 10) {
+                if (evBI >= 225) rollback = 85;
+                if (evBI >= 190 && evBI <= 224) rollback = 84;
+                if (evBI >= 176 && evBI <= 189) rollback = 82;
+                if (evBI >= 150 && evBI <= 175) rollback = 81;
+                if (evBI >= 120 && evBI <= 149) rollback = 75;
+                if (evBI >= 90 && evBI <= 119) rollback = 73;
+                if (evBI >= 66 && evBI <= 89) rollback = 66;
+                if (evBI >= 56 && evBI <= 65) rollback = 62;
+                if (evBI <= 55) rollback = 20;
+            }
+        }
+
+        if (mesh instanceof ClearProfit) {
+            rollback = 100;
+        }
+
+        model.setRollback(rollback);
+        return rollback;
+    }
+
+    public double requiredTourneys() {
+        double desiredProfit = model.getDesiredProfit();
+        double buyIn = model.getBuyIn();
+        double rollback = model.getRollback();
+        double rake = model.getRake();
+        double rakeback = model.getRakebackPct();
+        double dollarEVPerTourney = model.getExpDollarEVT();
+        double requiredTourneys = desiredProfit / (rollback / 100) / (buyIn * (rake / 100) * (rakeback / 100)
+                                                                      + dollarEVPerTourney) + 1;
+        model.setRequiredTourneys(requiredTourneys);
+        return requiredTourneys;
+    }
+
+    public double requiredHours() {
+        double requiredHours = model.getRequiredTourneys() / (model.getTables() * model.getTourneysPerTable());
+        model.setRequiredHours(requiredHours);
+        return requiredHours;
+    }
+
+    public double dollarPerHour() {
+        double dollarsPerHour = model.getDesiredProfit() / model.getRequiredHours();
+        model.setDollarPerHour(dollarsPerHour);
+        return dollarsPerHour;
+    }
+
+}
