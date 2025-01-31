43e036cdfb5e3c84f826b3c59e19cf3f3ce0d36f
Foge65 <mr.texnik666@gmail.com>
2025-01-31 23:55:16 +0300

    change port

--- web/vite.config.js
+++ web/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': {
-                target: 'http://localhost:8081',
+                target: 'http://localhost:8084',
                 changeOrigin: true
             }
         }
