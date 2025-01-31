4fc9d0f1ec6a6053359d0fc7fd87513fa8c9b924
Foge65 <mr.texnik666@gmail.com>
2025-01-31 23:10:37 +0300

    Docker containers working

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,6 +2,11 @@ import {defineConfig} from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
+    base: "/",
+    build: {
+        assetsDir: "assets",
+        manifest: true,
+    },
     plugins: [react()],
     server: {
         proxy: {
