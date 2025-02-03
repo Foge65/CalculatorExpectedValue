2c0027d6b14b645b7c3b7bf7609e19b1583d1570
Foge65 <mr.texnik666@gmail.com>
2025-02-04 02:19:46 +0300

    move table files to dir

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,7 +1,7 @@
 import {DesiredProfitDataProvider} from "./components/providers/DesiredProfitDataProvider.jsx";
-import DesiredProfitTable from "./components/DesiredProfitTable.jsx";
+import DesiredProfitTable from "./components/tables/DesiredProfitTable.jsx";
 import {HaveHoursDataProvider} from "./components/providers/HaveHoursDataProvider.jsx";
-import HaveHoursTable from "./components/HaveHoursTable.jsx";
+import HaveHoursTable from "./components/tables/HaveHoursTable.jsx";
 
 export default function App() {
     return (
--- web/src/components/tables/DesiredProfitTable.jsx
+++ web/src/components/tables/DesiredProfitTable.jsx
@@ -1,19 +1,19 @@
+import {Context} from "../providers/DesiredProfitDataProvider.jsx";
+import CreateColumn from "../CreateColumn.jsx";
+import {idsStruct, urlsStruct} from "../../data/DesiredProfit.js";
 import {useContext} from "react";
-import {Context} from "./providers/HaveHoursDataProvider.jsx";
-import CreateColumn from "./CreateColumn.jsx";
-import {idsStruct, urlsStruct} from "../data/HaveHours.js";
 
-export default function HaveHoursTable() {
+export default function DesiredProfitTable() {
     const {data, setData} = useContext(Context);
 
     return (
         <div className="tables">
             <table id="desiredProfitTable">
-                <caption><h3>Есть часов в месяц</h3></caption>
+                <caption><h3>Желаемый профит</h3></caption>
                 <tbody>
                 <CreateColumn idsStruct={idsStruct} urlsStruct={urlsStruct} data={data} setData={setData}/>
                 </tbody>
             </table>
         </div>
     );
-}
+};
